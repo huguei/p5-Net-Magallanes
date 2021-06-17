50f33dda298f6e8ae0979242c8db732eb4c46b17
Hugo Salgado <hsalgado@vulcano.cl>
2021-06-17 17:55:17 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Net::Magallanes - Blah blah blah
+
+# SYNOPSIS
+
+    use Net::Magallanes;
+
+# DESCRIPTION
+
+Net::Magallanes is
+
+# AUTHOR
+
+Hugo Salgado <hsalgado@vulcano.cl>
+
+# COPYRIGHT
+
+Copyright 2021- Hugo Salgado
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Net/Magallanes.pm
@@ -0,0 +1,39 @@
+package Net::Magallanes;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Net::Magallanes - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Net::Magallanes;
+
+=head1 DESCRIPTION
+
+Net::Magallanes is
+
+=head1 AUTHOR
+
+Hugo Salgado E<lt>hsalgado@vulcano.clE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2021- Hugo Salgado
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
